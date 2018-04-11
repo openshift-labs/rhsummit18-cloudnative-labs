d7fe5db9722e858623e09fa9dbfcdc5d777810f4
jamesfalkner <schtool@gmail.com>
2018-04-11 09:12:02 -0400

    use node.js for inventory to give programmatic control over service delay injection

---
+++ inventory/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/*
+ *
+ *  Copyright 2016-2017 Red Hat, Inc, and individual contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const inventorydb = JSON.parse(fs.readFileSync('inventory.json', 'utf8'));
+const SERVICE_DELAY = process.env.SERVICE_DELAY || 0;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/services/inventory/:itemId', (request, response) => {
+
+  const  itemId = request.params.itemId;
+
+  setTimeout(function() {
+
+    if (itemId === 'all') {
+      console.log(new Date() + " - GET /services/inventory/all: 200 OK");
+      return response.json(inventorydb);
+    }
+
+    const inventory_result = inventorydb.find(function(el) {
+      return el.itemId === itemId;
+    });
+    if (inventory_result) {
+      console.log(new Date() + " - GET /services/inventory/" + itemId + ": 200 OK");
+      return response.json(inventory_result);
+    } else {
+      console.log(new Date() + " - GET /services/inventory/" + itemId + ": 404 Not Found");
+      response.sendStatus(404).end();
+    }
+  }, SERVICE_DELAY);
+  
+});
+
+
+module.exports = app;
