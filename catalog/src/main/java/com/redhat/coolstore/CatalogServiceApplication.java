0105553766f30d935facd271c2c99ddcc5c64b72
Thomas Qvarnström <tqvarnst@redhat.com>
2018-04-30 15:16:04 +0200

    Added CORS Header to the base project

--- catalog/src/main/java/com/redhat/coolstore/CatalogServiceApplication.java
+++ catalog/src/main/java/com/redhat/coolstore/CatalogServiceApplication.java
@@ -1,7 +1,19 @@
 package com.redhat.coolstore;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
 
 @SpringBootApplication
 public class CatalogServiceApplication {
@@ -9,4 +21,23 @@ public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
+    
+    @Controller
+    class SimpleCORSFilter implements Filter {
+        public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+            HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            chain.doFilter(req, res);
+        }
+    
+        public void init(FilterConfig filterConfig) {
+        }
+    
+        public void destroy() {
+        }
+    
+    }
+    
 }
